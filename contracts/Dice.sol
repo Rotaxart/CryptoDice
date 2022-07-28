732c2217f17f51f7b7b545c9164287903d16f6bb
artem <rotaxart125@gmail.com>
2022-07-28 20:36:55 +0300

    Gasoptimisation

--- contracts/Dice.sol
+++ contracts/Dice.sol
@@ -15,15 +15,16 @@ contract Dice is DiceCoin{
     enum direction {
         UP, 
         DOWN, 
-        ZERO}
-    struct Player{
-        address payable playerAddress;
-        uint8[] rolls;
-        direction[] directions;
-        result[] results;
-    }
+        ZERO
+        }
+    // struct Player{
+    //     address payable playerAddress;
+    //     uint8[] rolls;
+    //     direction[] directions;
+    //     result[] results;
+    // }
 
-    mapping (address => Player) public players;
+    // mapping (address => Player) public players;
     
     constructor(){
         contractOwner = payable(msg.sender);
@@ -33,7 +34,8 @@ contract Dice is DiceCoin{
     event playLog(address player, 
                   uint number, 
                   direction _direction, 
-                  uint value
+                  uint value,
+                  result _result
                  );
     //Everyone can change salt value
     function setSalt(uint _salt) public returns (result){
@@ -41,67 +43,73 @@ contract Dice is DiceCoin{
         return result.OK;
     }
     //Generate random number
-    function getRandomNumber() view public returns (uint) {
+    function getRandomNumber() view internal returns (uint) {
         return uint(keccak256(abi.encodePacked((block.difficulty ^ cryptoSalt), block.timestamp)));
     }
 
-    function initialisation() public {
-        players[msg.sender].playerAddress = payable(msg.sender);
-    }
+    // function initialisation() public {
+    //     players[msg.sender].playerAddress = payable(msg.sender);
+    // }
 
-    function getPlayerInfo() public view returns (address, uint8, direction, result){
-        return (players[msg.sender].playerAddress, 
-                players[msg.sender].rolls[players[msg.sender].rolls.length - 1],
-                players[msg.sender].directions[players[msg.sender].directions.length - 1],
-                players[msg.sender].results[players[msg.sender].results.length - 1]) ;
-    }
+    // function getPlayerInfo() public view returns (address, uint8, direction, result){
+    //     return (players[msg.sender].playerAddress, 
+    //             players[msg.sender].rolls[players[msg.sender].rolls.length - 1],
+    //             players[msg.sender].directions[players[msg.sender].directions.length - 1],
+    //             players[msg.sender].results[players[msg.sender].results.length - 1]) ;
+    // }
 
     function playDice(direction _direction, uint _amount) public payable {
         require(_direction == direction.UP || 
                 _direction == direction.DOWN || 
                 _direction == direction.ZERO, 'Wrong value');
+        require(_amount > 0, 'Wrong value');
 
         uint8 randomNumber = uint8(getRandomNumber() % 101);
-        players[msg.sender].rolls.push(randomNumber);
-        players[msg.sender].directions.push(_direction);
-        
-        cryptoSalt++;
-         emit playLog(players[msg.sender].playerAddress, 
-                      randomNumber, 
-                      _direction, 
-                      msg.value
-                     );
+        // players[msg.sender].rolls.push(randomNumber);
+        // players[msg.sender].directions.push(_direction);
+        result _result;        
+         cryptoSalt++;
 
         if(_direction == direction.UP){
             if(randomNumber > 50){
-            //    players[msg.sender].playerAddress.transfer(msg.value * 2);
-                _approve(contractOwner, msg.sender, _amount);
-                transferFrom(contractOwner, msg.sender, _amount);
-                players[msg.sender].results.push(result.WIN);
+                // _approve(contractOwner, msg.sender, _amount);
+                // transferFrom(contractOwner, msg.sender, _amount);
+                _mint(msg.sender, _amount);
+                _result = result.WIN;
             }else{               
-                players[msg.sender].results.push(result.LOSE);
-                transfer(contractOwner, _amount);
+                // transfer(contractOwner, _amount);
+                _burn(msg.sender, _amount);
+                _result = result.LOSE;
             }
         }else if(_direction == direction.DOWN){
             if(randomNumber < 49){
-            //    players[msg.sender].playerAddress.transfer(msg.value * 2);
-                _approve(contractOwner, msg.sender, _amount);
-                transferFrom(contractOwner, msg.sender, _amount);
-                players[msg.sender].results.push(result.WIN);
+                // _approve(contractOwner, msg.sender, _amount);
+                // transferFrom(contractOwner, msg.sender, _amount);
+                _mint(msg.sender, _amount);
+                _result = result.WIN;
             }else{
-                players[msg.sender].results.push(result.LOSE);
-                transfer(contractOwner, _amount);
+                // transfer(contractOwner, _amount);
+                _burn(msg.sender, _amount);
+                _result = result.LOSE;
             }
         }else if(_direction == direction.ZERO){
             if(randomNumber == 49 || randomNumber == 50 ){
-             //   players[msg.sender].playerAddress.transfer(msg.value * 10);
-                _approve(contractOwner, msg.sender, _amount * 10);
-                transferFrom(contractOwner, msg.sender, _amount * 10);
-                players[msg.sender].results.push(result.WIN);
+                // _approve(contractOwner, msg.sender, _amount * 10);
+                // transferFrom(contractOwner, msg.sender, _amount * 10);
+                _mint(msg.sender, _amount * 10);
+                _result = result.WIN;
             }else{
-                players[msg.sender].results.push(result.LOSE);
-                transfer(contractOwner, _amount);
+                // transfer(contractOwner, _amount);
+                _burn(msg.sender, _amount);
+                _result = result.LOSE;
             }
-        }
+        }    
+        emit playLog(msg.sender,
+            randomNumber, 
+            _direction, 
+            _amount,
+            _result
+            );           
     }
 }
+
--- test/dice_test.js
+++ test/dice_test.js
@@ -1,132 +1,179 @@
 const Dice = artifacts.require('Dice');
-
+const truffleAssert = require('truffle-assertions');
 contract('Dice', accounts => {
-    it('salt should changed', async () => {
+    it('Salt should changed', async () => {
         let instance = await Dice.deployed();
-        let salt = 0xffff;
+        let salt = 0xffff; //salt
         let play = await instance.setSalt(salt);
         let cryptoSalt = await instance.cryptoSalt();
         console.log(`New salt: ${cryptoSalt}`);
         assert.equal(cryptoSalt, salt);
     })
-    it('player state should changed', async () => {
-        let instance = await Dice.deployed();
-        //let log = await instance.getPlayerInfo();
-        let init = await instance.initialisation();
-        let play = await instance.playDice(0, 1);
-        let log1 = await instance.getPlayerInfo();
-        
-        console.log(`random number: ${log1[1]}`);
-        assert.notEqual(log1[1], 0);
-    })
-    it('address should added to players struct', async () => {
-        let instance = await Dice.deployed();
-        //let log = await instance.getPlayerInfo();
-        let init = await instance.initialisation();
-        let address = await instance.getPlayerInfo();
 
-        console.log(`Assress: ${address[0]}`);
-        assert.equal(address[0], accounts[0]);
-    })
     it('Result is correctly', async () => {
         let instance = await Dice.deployed();
-        let init = await instance.initialisation();
         let transfer = await instance.transfer(accounts[1], 10000);
-        let play = await instance.playDice(0, 1, {from: accounts[1], gas: 5000000});
+        let play = await instance.playDice(0, 1, {from: accounts[1]});
 
-        let log1 = await instance.getPlayerInfo({from: accounts[1]});
+        // let log1 = await instance.getPlayerInfo({from: accounts[1]});
         //log1 = await instance.getPlayerInfo();
+        let log1 = await instance.getPastEvents('playLog',{});
+
+        // console.log(log1[0].returnValues.number);
+        
         for(let i=0; i<1; ){
-            if(log1[1] > 50){
+            if(log1[0].returnValues.number > 50){
                 i++;
             }
             else {
-                  play = await instance.playDice(0, 1, {from: accounts[1], gas: 5000000});
-                  log1 = await instance.getPlayerInfo({from: accounts[1]});
+                  play = await instance.playDice(0, 1, {from: accounts[1]});
+                  log1 = await instance.getPastEvents('playLog',{});
             }
         }
         
-        log1= await instance.getPlayerInfo({from: accounts[1]});;       
-        console.log(`random number: ${log1[1]}; direction: ${log1[2]}; result: ${log1[3]}; Win`);
-        assert.equal(log1[3], 2);
-        play = await instance.playDice(0, 1, {from: accounts[1], gas: 5000000});
-        log1 = await instance.getPlayerInfo({from: accounts[1]});
+        log1 = await instance.getPastEvents('playLog',{});       
+        console.log(`
+            random number: ${log1[0].returnValues.number}; 
+            direction: ${log1[0].returnValues._direction}; 
+            result: ${log1[0].returnValues._result}: Win`);
+        assert.equal(log1[0].returnValues._result, 2);
+        play = await instance.playDice(0, 1, {from: accounts[1]});
+        log1 = await instance.getPastEvents('playLog',{});
+
         for(let i=0; i<1; ){
-            if(log1[1] <= 50){
+            if(log1[0].returnValues.number <= 50){
                 i++;
             }
             else {
-                  play = await instance.playDice(0, 1, {from: accounts[1], gas: 5000000});
-                  log1 = await instance.getPlayerInfo({from: accounts[1]});
+                  play = await instance.playDice(0, 1, {from: accounts[1]});
+                  log1 = await instance.getPastEvents('playLog',{});
             }
         }
         
-        log1= await instance.getPlayerInfo({from: accounts[1]});;       
-        console.log(`random number: ${log1[1]}; direction: ${log1[2]}; result: ${log1[3]}; Lose`);
-        assert.equal(log1[3], 3);
+        log1 = await instance.getPastEvents('playLog',{});       
+        console.log(`
+            random number: ${log1[0].returnValues.number}; 
+            direction: ${log1[0].returnValues._direction}; 
+            result: ${log1[0].returnValues._result}: Lose`);
+        assert.equal(log1[0].returnValues._result, 3);
         
-        play = await instance.playDice(1, 1, {from: accounts[1], gas: 5000000});  
-        log1 = await instance.getPlayerInfo({from: accounts[1]});
+        play = await instance.playDice(1, 1, {from: accounts[1]});  
+        log1 = await instance.getPastEvents('playLog',{});
         for(let i=0; i<1; ){
-            if(log1[1] < 49){
+            if(log1[0].returnValues.number < 49){
                 i++;
             }
             else {
-                  play = await instance.playDice(1, 1, {from: accounts[1], gas: 5000000});
-                  log1 = await instance.getPlayerInfo({from: accounts[1]});
+                  play = await instance.playDice(1, 1, {from: accounts[1]});
+                  log1 = await instance.getPastEvents('playLog',{});
             }
         }
         
-        log1= await instance.getPlayerInfo({from: accounts[1]});;       
-        console.log(`random number: ${log1[1]}; direction: ${log1[2]}; result: ${log1[3]}: Win`);
-        assert.equal(log1[3], 2);
+        log1 = await instance.getPastEvents('playLog',{});       
+        console.log(`
+            random number: ${log1[0].returnValues.number}; 
+            direction: ${log1[0].returnValues._direction}; 
+            result: ${log1[0].returnValues._result}: Win`);
+        assert.equal(log1[0].returnValues._result, 2);
         
-        play = await instance.playDice(1, 1, {from: accounts[1], gas: 5000000});
-        log1 = await instance.getPlayerInfo({from: accounts[1]});
+        play = await instance.playDice(1, 1, {from: accounts[1]});
+        log1 = await instance.getPastEvents('playLog',{});
         for(let i=0; i<1; ){
-            if(log1[1] >= 49){
+            if(log1[0].returnValues.number >= 49){
                 i++;
             }
             else {
-                  play = await instance.playDice(1, 1, {from: accounts[1], gas: 5000000});
-                  log1 = await instance.getPlayerInfo({from: accounts[1]});
+                  play = await instance.playDice(1, 1, {from: accounts[1]});
+                  log1 = await instance.getPastEvents('playLog',{});
             }
         }
         
-        log1= await instance.getPlayerInfo({from: accounts[1]});;       
-        console.log(`random number: ${log1[1]}; direction: ${log1[2]}; result: ${log1[3]}: Lose`);
-        assert.equal(log1[3], 3);
+        log1 = await instance.getPastEvents('playLog',{});       
+        console.log(`
+            random number: ${log1[0].returnValues.number}; 
+            direction: ${log1[0].returnValues._direction}; 
+            result: ${log1[0].returnValues._result}: Lose`);
+        assert.equal(log1[0].returnValues._result, 3);
 /*
         play = await instance.playDice(2);
-        log1 = await instance.getPlayerInfo();
+        log1 = await instance.getPastEvents('playLog',{});
         for(let i=0; i<1; ){
-            if(log1[1] == 49 || log1[1] == 50){
+            if(log1[0].returnValues.number == 49 || log1[1] == 50){
                 i++;
             }
             else {
                   play = await instance.playDice(2);
-                  log1 = await instance.getPlayerInfo();
+                  log1 = await instance.getPastEvents('playLog',{});
             }
         }
         
-        log1= await instance.getPlayerInfo();;       
-        console.log(`random number: ${log1[1]}; direction: ${log1[2]}; result: ${log1[3]}: Win`);
-        assert.equal(log1[3], 2);
+        log1 = await instance.getPastEvents('playLog',{});       
+        console.log(`
+            random number: ${log1[0].returnValues.number}; 
+            direction: ${log1[0].returnValues._direction}; 
+            result: ${log1[0].returnValues._result}: Win`);
+        assert.equal(log1[0].returnValues._result, 2);
 */        
-        play = await instance.playDice(2, 1, {from: accounts[1], gas: 5000000});
-        log1 = await instance.getPlayerInfo({from: accounts[1]});
+        play = await instance.playDice(2, 1, {from: accounts[1]});
+        log1 = await instance.getPastEvents('playLog',{});
         for(let i=0; i<1; ){
-            if(log1[1] != 49 || log1[1] != 50){
+            if(log1[0].returnValues.number != 49 && log1[0].returnValues.number != 50){
                 i++;
             }
             else {
-                  play = await instance.playDice(1, 1, {from: accounts[1], gas: 5000000});
-                  log1 = await instance.getPlayerInfo({from: accounts[1]});
+                  play = await instance.playDice(1, 1, {from: accounts[1]});
+                  log1 = await instance.getPastEvents('playLog',{});
             }
         }
         
-        log1= await instance.getPlayerInfo({from: accounts[1]});;       
-        console.log(`random number: ${log1[1]}; direction: ${log1[2]}; result: ${log1[3]}: Lose`);
-        assert.equal(log1[3], 3);
-    })   
+        log1 = await instance.getPastEvents('playLog',{});       
+        console.log(`
+            random number: ${log1[0].returnValues.number}; 
+            direction: ${log1[0].returnValues._direction}; 
+            result: ${log1[0].returnValues._result}: Lose`);
+        assert.equal(log1[0].returnValues._result, 3);
+    })  
+    it('Balance should changed', async () => {
+        let instance = await Dice.deployed();
+        let transfer = await instance.transfer(accounts[1], 10000);
+        let play = await instance.playDice(0, 1, {from: accounts[1]});
+        let log1 = await instance.getPastEvents('playLog',{});
+        let balance = await instance.balanceOf(accounts[1]);
+        for (let i = 0; i < 2;){
+            let balance1 = await instance.balanceOf(accounts[1]);
+            play = await instance.playDice(0, 1, {from: accounts[1]});
+            log1 = await instance.getPastEvents('playLog',{});
+            if(i == 0 && log1[0].returnValues._result == 2){
+                balance = await instance.balanceOf(accounts[1]);
+                console.log(balance);
+                assert(balance1 < balance);
+                i++;
+            }
+            if(i == 1 && log1[0].returnValues._result == 3){
+                balance = await instance.balanceOf(accounts[1]);
+                console.log(balance);
+                assert(balance1 > balance);
+                i++;
+            }
+        }
+    }) 
+    it('200 rolls', async () => {
+        let instance = await Dice.deployed();
+        let transfer = await instance.transfer(accounts[1], 10000);
+        let play = await instance.playDice(0, 1, {from: accounts[1]});
+        let log1 = await instance.getPastEvents('playLog',{});
+        let balance = await instance.balanceOf(accounts[1]);
+        console.log(`balance before: ${balance}`);
+
+        let wins = 0;
+        let loses = 0;
+        for (let i = 0; i < 200; i++){
+            play = await instance.playDice(0, 1, {from: accounts[1]});
+            log1 = await instance.getPastEvents('playLog',{});
+            if(log1[0].returnValues._result == 2) wins++;
+            if(log1[0].returnValues._result == 3) loses++;
+        }
+        balance = await instance.balanceOf(accounts[1]);
+        console.log(`balance after: ${balance}, wins: ${wins}, loses: ${loses}`);
+    })
 })
